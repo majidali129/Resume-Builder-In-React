4ad0eb46fd8e294270bee62cb8849bef0b79216d
Majid Ali <itsmajidali129@gmail.com>
2023-03-01 22:34:45 +0500

    setup done

---
+++ src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+             <h1 className='text-red text-3xl text-center'>This is my first project using tailwind.</h1>
+    </div>
+  )
+}
+
+export default Welcome
